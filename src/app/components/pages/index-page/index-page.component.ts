5667eba1cfd08b036cd710ae2fd60c2b35cc9caf
Peter Gál <pergalsk@gmail.com>
2023-11-25 21:05:39 +0100

    Dev: removed unnecessary constructor and OnInit interface implementation

--- src/app/components/pages/index-page/index-page.component.ts
+++ src/app/components/pages/index-page/index-page.component.ts
@@ -1,14 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-index-page',
   templateUrl: './index-page.component.html',
   styleUrls: ['./index-page.component.scss'],
 })
-export class IndexPageComponent implements OnInit {
+export class IndexPageComponent {
   formIds: number[] = [13405, 16498, 999];
-
-  constructor() {}
-
-  ngOnInit(): void {}
 }
