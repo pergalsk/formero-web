d05d80ff95c5164dbc92330b84d8d828e488f910
Peter Gál <pergalsk@gmail.com>
2024-01-06 23:32:24 +0100

    Fixed: global layout - max page width

--- src/app/components/layouts/with-menu-layout/with-menu-layout.component.ts
+++ src/app/components/layouts/with-menu-layout/with-menu-layout.component.ts
@@ -14,51 +14,49 @@ import { NgxColorSchemesComponent } from 'ngx-color-schemes';
     NgxColorSchemesComponent,
   ],
   template: `
-    <div class="content">
-      <div class="flx-row flx-end">
-        <app-auth-panel></app-auth-panel>
-        <ngx-color-schemes size="28"></ngx-color-schemes>
-      </div>
-
-      <header>
-        <h1>Formero APP</h1>
-
-        <nav>
-          <ul>
-            <li>
-              <a
-                [routerLink]="['']"
-                [routerLinkActive]="['active-link']"
-                [routerLinkActiveOptions]="{ exact: true }"
-                >Domov</a
-              >
-            </li>
-            <li>Formuláre:</li>
-            <ul>
-              @for (id of formIds; track $index) {
-                <li>
-                  <a [routerLink]="['/form', id]" [routerLinkActive]="['active-link']">
-                    Formulár id = {{ id }}
-                  </a>
-                </li>
-              }
-            </ul>
-            <li>
-              <a [routerLink]="['/about']" [routerLinkActive]="['active-link']">O aplikácii</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
-
-      <main>
-        <router-outlet></router-outlet>
-      </main>
-
-      <footer>
-        <hr />
-        <span>Copyright (c) 2022 | -pg-</span>
-      </footer>
+    <div class="flx-row flx-end">
+      <app-auth-panel></app-auth-panel>
+      <ngx-color-schemes size="28"></ngx-color-schemes>
     </div>
+
+    <header>
+      <h1>Formero APP</h1>
+
+      <nav>
+        <ul>
+          <li>
+            <a
+              [routerLink]="['']"
+              [routerLinkActive]="['active-link']"
+              [routerLinkActiveOptions]="{ exact: true }"
+              >Domov</a
+            >
+          </li>
+          <li>Formuláre:</li>
+          <ul>
+            @for (id of formIds; track $index) {
+              <li>
+                <a [routerLink]="['/form', id]" [routerLinkActive]="['active-link']">
+                  Formulár id = {{ id }}
+                </a>
+              </li>
+            }
+          </ul>
+          <li>
+            <a [routerLink]="['/about']" [routerLinkActive]="['active-link']">O aplikácii</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+
+    <main>
+      <router-outlet></router-outlet>
+    </main>
+
+    <footer>
+      <hr />
+      <span>Copyright (c) 2022 | -pg-</span>
+    </footer>
   `,
 })
 export class WithMenuLayoutComponent {
