d1d15af761ffe379211dc1fb3579f72f375c9690
Peter Gál <pergalsk@gmail.com>
2024-04-08 22:25:03 +0200

    Dev: optimized component export from one directory

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,8 +1,11 @@
-export * from './blocks/formero-textbox/formero-textbox.component';
-export * from './blocks/formero-textarea/formero-textarea.component';
 export * from './common/formero-field-messages/formero-field-messages.component';
-export * from './blocks/formero-dropdown/formero-dropdown.component';
-export * from './blocks/formero-radiogroup/formero-radiogroup.component';
-export * from './blocks/formero-agreement/formero-agreement.component';
+
 export * from './blocks/formero-title/formero-title.component';
 export * from './blocks/formero-blocktext/formero-blocktext.component';
+export * from './blocks/formero-textbox/formero-textbox.component';
+export * from './blocks/formero-textarea/formero-textarea.component';
+export * from './blocks/formero-dropdown/formero-dropdown.component';
+export * from './blocks/formero-checkgroup/formero-checkgroup.component';
+export * from './blocks/formero-radiogroup/formero-radiogroup.component';
+export * from './blocks/formero-agreement/formero-agreement.component';
+export * from './blocks/formero-validation/formero-validation.component';
