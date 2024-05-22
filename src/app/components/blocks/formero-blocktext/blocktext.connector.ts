0036e6b5a4e7521fd43c8bbce1c739ae5343fb47
Peter Gál <pergalsk@gmail.com>
2024-05-22 23:24:35 +0200

    New: blocktext connected to service form

--- src/app/components/blocks/formero-blocktext/blocktext.connector.ts
+++ src/app/components/blocks/formero-blocktext/blocktext.connector.ts
@@ -1,10 +1,12 @@
 import { inject, Injectable } from '@angular/core';
 import { BlockGroupType } from '@app/schema/schema';
 import { UtilsService } from '@services/utils.service';
+import { BlocksGalleryService } from '@services/blocks-gallery.service';
 
 @Injectable({ providedIn: 'root' })
 export class BlocktextConnector {
   utilsService: UtilsService = inject(UtilsService);
+  blockGallery: BlocksGalleryService = inject(BlocksGalleryService);
 
   private readonly type = 'blocktext';
   private readonly uiTitle = 'Odstavec';
@@ -30,13 +32,21 @@ export class BlocktextConnector {
   getServiceBlocks() {
     return [
       {
-        type: 'textbox',
+        type: 'textarea',
         key: 'content',
         label: 'Text odstavca',
         value: this.defaults.content,
         validators: [{ type: 'maxLength', params: [5000] }],
         layout: { panel: 1 },
       },
+      this.blockGallery.layout(1),
     ];
+  }
+
+  transform(value: any) {
+    return {
+      ...value,
+      layout: { panel: parseInt(value.layout, 10) ?? 1 },
+    };
   }
 }
